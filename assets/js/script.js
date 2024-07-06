7da204c62034c057eaf761a675d96b29d52fbddb
Doaa Al-metwaly <doaatarek127@gmail.com>
2024-07-07 00:03:37 +0300

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
 
-        items:1
-    });
-  });
+icon = document.querySelector('.icon');
+icon.onclick=function(){
+    navBar=document.querySelector('.nav-bar');
+    navBar.classList.toggle('active');
+}
+  
+//      $(document).ready(function () {
+//        $(".carousel_se_01_carousel").owlCarousel({
+//          items: 1,
+       
+       
+//      });
+//  });
+ $(document).ready(function () {
+     $(".carousel_se_02_carousel").owlCarousel({
+       items: 1,
+      nav:true,
+      dots:false
+   });
+ });
+
+ 

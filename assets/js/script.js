2d49f5d90c40e2d7d356ac05ec20d01a5bbf356c
Doaa Al-metwaly <doaatarek127@gmail.com>
2024-07-07 14:39:19 +0300

    last update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,57 @@
-
+/**toggel button */
 icon = document.querySelector('.icon');
 icon.onclick=function(){
     navBar=document.querySelector('.nav-bar');
     navBar.classList.toggle('active');
 }
   
-//      $(document).ready(function () {
-//        $(".carousel_se_01_carousel").owlCarousel({
-//          items: 1,
-       
-       
-//      });
-//  });
- $(document).ready(function () {
-     $(".carousel_se_02_carousel").owlCarousel({
-       items: 1,
-      nav:true,
-      dots:false
+/**carsouel */
+ if ($("#slider-recruitment").length) {
+  $("#slider-recruitment").owlCarousel({
+   loop: true,
+   margin: 10,
+   nav: false,
+   items: 5,
+   autoplayTimeout: 3500,
+  autoplayHoverPause: false,
+   rtl: true,
+  autoplay: true,
+  
+   autoplayHoverPause: true,
+   dots: true,
+   smartSpeed: 500,
+   responsiveClass: true,
+    responsive: {
+   0: {
+   items: 1,
+   },
+   },
    });
- });
+  }
+  if ($("#carousel_se_02_carousel").length) {
+    $("#carousel_se_02_carousel").owlCarousel({
+     loop: true,
+     margin: 20,
+     nav:true,
+    dots:false,
+     items: 3,
+     autoplayTimeout: 3500,
+    autoplayHoverPause: false,
+     rtl: true,
+    autoplay: true,
+    
+     autoplayHoverPause: true,
+     smartSpeed: 700,
+     responsiveClass: true,
+      responsive: {
+     0: {
+     items: 1,
+     },
+     },
+     });
+    }
+  /** language*/
 
- 
+function changeLanguage(lang) {
+  alert(lang === 'en' ? 'Language changed to English.' : 'تم تغيير اللغة إلى العربية');
+}
